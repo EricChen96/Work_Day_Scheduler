3c2522e236b2149f198477d888e9110431b79cd3
Eric Chen <ericchen201896@gmail.com>
2020-12-17 10:15:59 -0800

    Added in for loop that loops through every hour and will make it past, present, or future class

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ $(function () {
 
     var currentHourTime = parseInt(moment().format('k'));
 
- 
+    //Change color
+    for (var hourCount = 9; hourCount < 18; hourCount++) {
+        var hourgrabEl = $("#hour-" + hourCount);
+        if (hourCount < currentHourTime) {
+            hourgrabEl.attr("class", "row time-block past");
+        }
+        else if (hourCount === currentHourTime) {
+            hourgrabEl.attr("class", "row time-block present");
+        }
+        else {
+            hourgrabEl.attr("class", "row time-block future");                                                       
+        }
+    }
+
+
 
 
     console.log(moment().format('k'));
