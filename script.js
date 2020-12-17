a4c5e06625884cbf6e59ab2b6cdb642e4ac01053
Eric Chen <ericchen201896@gmail.com>
2020-12-17 14:18:32 -0800

    Added in the save button actionlistener. This takes the description for the corresponding hour and saves it as an object with hour:description. Then sends it to local storage to save the object

--- script.js
+++ script.js
@@ -17,13 +17,37 @@ $(function () {
         else {
             hourgrabEl.attr("class", "row time-block future");                                                       
         }
+    };
+
+    //Saving description
+    var descriptionArray = {};
+    $(".saveBtn").on("click", function () {
+        var tempString = $(this.previousElementSibling).val();
+        var hourTemp = $(this).val();
+        descriptionArray[hourTemp] = tempString;
+        localStorage.setItem("description", JSON.stringify(descriptionArray));
+        // console.log(descriptionArray[17]);
+    });
+
+    function displaySavedDescriptions() {
+        for(var hourCount = 9; hourCount < 18; hourCount++) {
+            if(descriptionArray[hourCount] !== undefined) {
+                // $(this.previousElementSibling).text(descriptionArray[hourCount]);
+                var hourgrabEl = $("#hour-" + hourCount);
+                console.log(descriptionArray[hourCount]);
+                hourgrabEl.$("textarea").val(descriptionArray[hourCount]);
+            }
+        }
     }
 
+    function init() {
+        var storedDescriptions = JSON.parse(localStorage.getItem("description"));
+        if (storedDescriptions !== null) {
+            descriptionArray = storedDescriptions;
+        }
+        displaySavedDescriptions();
+    };
 
-
-
-    console.log(moment().format('k'));
-    console.log(currentHourTime);
-    console.log(typeof (currentHourTime));
+    init();
 })
 
